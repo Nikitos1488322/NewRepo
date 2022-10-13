048d00052ec66e66dd6e4ff6cfcced8d6cc0988a
Nikitos1488322 <115684779+Nikitos1488322@users.noreply.github.com>
2022-10-13 13:35:12 +0800

    Add files via upload

---
+++ repos/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Калькулятор
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+            private void button1_Click(object sender, EventArgs e)
+            {
+                double Nalog = 0, Vichet = 0, Deti = 0, D = 0, Dsumm = 0, r = 0;
+                try
+                {
+                    D = Convert.ToDouble(textBox1.Text);
+                    Dsumm = Convert.ToDouble(textBox2.Text);
+                }
+                catch (Exception ex)
+                {
+                    r++;
+                    MessageBox.Show(ex.ToString(), "Ошибка! Некорректный ввод данных в поле доходов физ. лица за 1 месяц", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                try
+                {
+                    Dsumm = Convert.ToDouble(textBox2.Text);
+                }
+                catch (Exception ex)
+                {
+                    r++;
+                    MessageBox.Show(ex.ToString(), "Ошибка! Некорректный ввод данных в поле доходов физ. лица с начала года", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                try
+                {
+                    Deti = Convert.ToDouble(textBox3.Text);
+                }
+                catch (Exception ex)
+                {
+                    r++;
+                    MessageBox.Show(ex.ToString(), "Ошибка! Некорректный ввод данных в поле доходов физ. лица с начала года", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+          
+                if (Dsumm < 40000)
+                {
+                    if (checkBox1.Checked == true && Deti > 0)
+                        Vichet = 600 + 600 * Deti;
+                    else if (checkBox1.Checked == false && Deti > 0)
+                        Vichet = 600 + 1200 * Deti;
+                    Nalog = 0.13 * (D - Vichet);
+                }
+                else if (Dsumm >= 40000)
+                    Nalog = 0.13 * D;
+                if (r == 0)
+                {
+                    textBox4.Text = "Налоги составили: " + Convert.ToString(Nalog) + Environment.NewLine;
+                    textBox4.Text += "Чистый доход за месяц: " + Convert.ToString(D - Nalog);
+                }
+
+            }
+
+            private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                char number = e.KeyChar;
+                if (!Char.IsDigit(number) && number != 8 && number != 44)
+                {
+                    e.Handled = true;
+                }
+            }
+
+            private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                char number = e.KeyChar;
+                if (!Char.IsDigit(number) && number != 8 && number != 44)
+                {
+                    e.Handled = true;
+                }
+            }
+
+            private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                char number = e.KeyChar;
+                if (!Char.IsDigit(number) && number != 8 && number != 44)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+    }
+
---
+++ repos/Калькулятор/Калькулятор/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Калькулятор
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(241, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(241, 100);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(105, 51);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(241, 174);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(105, 43);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 309);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(218, 91);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(256, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Доход за 1 месяц";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 51);
+            this.label2.TabIndex = 6;
+            this.label2.Text = " Доход физического лица. Уже полученный им с начала года до месяца для которого п" +
+    "роизводится расчёт";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 43);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Количество детей до 18 лет, имеющихся на иждевении у физ лица";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 48);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Сведения о том, является ли семья полной";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Результат";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(282, 258);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
